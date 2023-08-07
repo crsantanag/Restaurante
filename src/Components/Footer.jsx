10a14efda27c59ad2498b12430fb89f838b5b6c0
Carlos Santana <carlos.santanag@live.cl>
2023-08-07 17:48:59 -0400

    Arregla Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -16,23 +16,7 @@ export const Footer = () => {
                 de carbón- con una fórmula novedosa de entender el acto de comer, o simplemente, 
                 disfrutar de un entorno agradable, moderno y desenfadado.
                 </p>
-                <div className="mt-4">
-                  <a
-                    type="button"
-                    className="btn btn-floating btn-light btn-lg" >
-                    <i className="fab fa-facebook-f"></i>
-                  </a>
-                  <a
-                    type="button"
-                    className="btn btn-floating btn-light btn-lg" >
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                  <a
-                    type="button"
-                    className="btn btn-floating btn-light btn-lg" >
-                    <i className="fab fa-google-plus-g"></i>
-                  </a>
-                </div>
+                
               </div>
 
               <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
@@ -40,23 +24,14 @@ export const Footer = () => {
                 <ul className="fa-ul">
                   {" "}
                   <li className="mb-3">
-                    <span className="fa-li">
-                      <i className="fas fa-home"></i>
-                    </span>
                     <span className="ms-2">
                       Av. Libertador Bernardo OHiggins 214, Concepción - Chile
                     </span>
                   </li>
                   <li className="mb-3">
-                    <span className="fa-li">
-                      <i className="fas fa-envelope"></i>
-                    </span>
                     <span className="ms-2">contacto@santana.com</span>
                   </li>
                   <li className="mb-3">
-                    <span className="fa-li">
-                      <i className="fas fa-phone"></i>
-                    </span>
                     <span className="ms-2">+ 56 41 5555555</span>
                   </li>
                 </ul>
@@ -66,7 +41,7 @@ export const Footer = () => {
                 <h5 className="text-uppercase mb-4">HORARIOS</h5>
 
                 <table className="table text-center text-white">
-                  <tbody className="fw-normal">
+                  <tbody className="fw-normal" style={{ textAlign: 'left', alignItems: 'left',  justifyContent: 'left' }}>
                     <tr>
                       <td>Lunes - Jueves</td>
                       <td>19:00 - 24.00 hrs.</td>
