8e44e52ca81359e0415e9d72154e49418383e950
Carlos Santana <carlos.santanag@live.cl>
2023-08-17 10:25:50 -0400

    Elimina comentarios console.log

--- src/Components/BotonRestar.jsx
+++ src/Components/BotonRestar.jsx
@@ -2,18 +2,18 @@ import './Boton.css'
 
 export const BotonRestar = ( { comensales, setComensales } ) => {
     
-    console.log ('*** RESTAR - Comensales antes de la función : ', comensales)
+    // console.log ('*** RESTAR - Comensales antes de la función : ', comensales)
     
     const restarUno = () => {
-        console.log ('RESTAR - ^^^^^^^^^^^^^^^^^^^^^^^^')
-        console.log ('RESTAR - Comensales antes de cambiar de estado : ', comensales)
+        // console.log ('RESTAR - ^^^^^^^^^^^^^^^^^^^^^^^^')
+        // console.log ('RESTAR - Comensales antes de cambiar de estado : ', comensales)
 
         if (comensales == 1) return;
 
         setComensales (contador => contador - 1)
 
-        console.log ('RESTAR - Comensales después de cambiar el estado : ', comensales)
-        console.log ('RESTAR - vvvvvvvvvvvvvvvvvvvvvvvvv')
+        // console.log ('RESTAR - Comensales después de cambiar el estado : ', comensales)
+        // console.log ('RESTAR - vvvvvvvvvvvvvvvvvvvvvvvvv')
     }
 
     return (
--- src/Components/BotonSumar.jsx
+++ src/Components/BotonSumar.jsx
@@ -2,18 +2,18 @@ import './Boton.css'
 
 export const BotonSumar = ( { comensales, setComensales } ) => {
     
-    console.log ('*** SUMAR - Comensales antes de la función : ', comensales)
+    // console.log ('*** SUMAR - Comensales antes de la función : ', comensales)
     
     const sumarUno = () => {
-        console.log ('SUMAR - ^^^^^^^^^^^^^^^^^^^^^^^^')
-        console.log ('SUMAR - Comensales antes de cambiar de estado : ', comensales)
+        // console.log ('SUMAR - ^^^^^^^^^^^^^^^^^^^^^^^^')
+        // console.log ('SUMAR - Comensales antes de cambiar de estado : ', comensales)
 
         if (comensales == 12) return;
 
         setComensales (contador => contador + 1)
 
-        console.log ('SUMAR - Comensales después de cambiar el estado : ', comensales)
-        console.log ('SUMAR - vvvvvvvvvvvvvvvvvvvvvvvvv')
+        // console.log ('SUMAR - Comensales después de cambiar el estado : ', comensales)
+        // console.log ('SUMAR - vvvvvvvvvvvvvvvvvvvvvvvvv')
     }
     
     return (
